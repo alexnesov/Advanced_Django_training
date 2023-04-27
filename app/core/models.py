5a8311c99e4fe758953508123e3ef468ae82eb71
alexnesov <alexandrenesovic@gmail.com>
2023-04-27 13:18:24 +0200

    bucket police added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,26 @@
+"""
+Tests for models
+"""
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successfull(self):
+        """Test creating a user with an email is succeessful"""
+
+        email = "test@example.com"
+        password = "testpass123"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.checkpassword(password))
