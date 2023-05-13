50ca94acff43ffddcb52e844cdfcbd9b9d098332
alexnesov <alexandrenesovic@gmail.com>
2023-05-13 14:40:50 +0200

    working user create in api docs

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -27,16 +27,15 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
     
-
     def update(self, instance, validated_data):
-        """Update and return user"""
+        """Update and return user."""
         password = validated_data.pop('password', None)
         user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
             user.save()
-        
+
         return user
 
 
--- app/user/views.py
+++ app/user/views.py
@@ -21,14 +21,12 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManagerUserView(generics.RetrieveUpdateAPIView):
-    """Manager the authenticated user."""
-
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
-
         return self.request.user
